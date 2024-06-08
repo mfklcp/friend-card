11eb85c117af1202cc1f7ab150e9299df7c4f735
Marcio Franklin <mfklcp@gmail.com>
2024-06-08 15:03:32 -0300

    create project

---
+++ app/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.jetbrains.kotlin.serialization)
+}
+
+android {
+    namespace = "com.mfkl.friendcard"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.mfkl.friendcard"
+        minSdk = 31
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(libs.bundles.android)
+    implementation(libs.bundles.jetbrains)
+    testImplementation(libs.bundles.test)
+    androidTestImplementation(libs.bundles.instrumented.test)
+}
